e9e61c50debbe12e915dc3072bf52a6c1a52d0f1
Samantha-Grace <sgrace732@gmail.com>
2022-03-11 11:45:15 -0700

    practice variables

--- hello/src/variables/one.rs
+++ hello/src/variables/one.rs
@@ -1,3 +1,5 @@
+
+// RUST: Safety Concurrency, Speedinitialize a single variable
 // no type annotations
 fn main() {
     let bunnies = 2;
@@ -8,3 +10,14 @@ fn main() {
     let bunnies: i32 =4;
 }
 
+// let statement allows us to initialize multiple variables at once
+// let statement can destructure data on right hand side 
+// and use it to initialize variables inside of a corresponding pattern on the left side
+fn main() {
+    let(bunnies, carrots) = (8, 50);
+}
+
+// variables are immutable by default
+fn main() {
+    let bunnies = 16;
+}
