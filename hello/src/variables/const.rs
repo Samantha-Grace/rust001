410b4e47cd82409269fae7214d92bdc4d583508c
Samantha-Grace <sgrace732@gmail.com>
2022-03-14 10:13:22 -0600

    const variables

---
+++ hello/src/variables/const.rs
@@ -0,0 +1,6 @@
+// const
+
+// use all caps (screaming snake case), mult words seprated by underscores 
+// type annotation is required
+// value must be a constant expression
+const MAX_POINTS: u32 = 100_000;
