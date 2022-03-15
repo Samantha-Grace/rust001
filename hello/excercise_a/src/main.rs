2820fd91ab5489c3814a9267b26b5e4e0ec6ffe6
Samantha-Grace <sgrace732@gmail.com>
2022-03-15 14:13:52 -0600

    i think this is called global scope -moving const out of block so it is available in any future function

--- hello/excercise_a/src/main.rs
+++ hello/excercise_a/src/main.rs
@@ -1,6 +1,8 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-    const STARTING_MISSILES: i32 = 8;
-    const READY_AMOUNT: i32 = 2;
+
     let mut missiles = STARTING_MISSILES;
     let ready = READY_AMOUNT;
 
