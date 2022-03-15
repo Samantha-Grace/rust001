d7bfbee3643ff94a2584f4796fd27325d5ae3577
Samantha-Grace <sgrace732@gmail.com>
2022-03-15 16:15:03 -0600

    am I fizzbuzzing yet?

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,15 +1,26 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
+// fn main(keyword: i32) {
+
+//     if keyword % 3 == 0 {
+//         print!("fizz");
+//     } else if keyword % 5 == 0  {
+//         print!("buzz");
+//     } else if keyword % 3 == 0 && keyword % 5 ==0 {
+//         print!("fizzbuzz");
+//     } else {
+//         print!("{}", keyword);
+//     }
+//     }
 
 fn main() {
-    let keyword = 100;
+    let number = 100;
 
-    if keyword >= 0 {
-        println!("fizz");
-    } else if keyword <= 0 {
-        println!("buzz");
+    if number % 3 == 0 {
+        print!("fizz");
+    } else if number % 5 == 0  {
+        print!("buzz");
+    } else if number % 3 == 0 && number % 5 ==0 {
+        print!("fizzbuzz");
     } else {
-        println!("fizzbuzz");
+        print!("{}", number);
     }
     }
