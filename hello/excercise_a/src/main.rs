71ac7f764a9f1e208b2f155c0f359395100e576e
Samantha-Grace <sgrace732@gmail.com>
2022-03-15 14:06:51 -0600

    subtract ready from missiles and make missiles variable mutable

--- hello/excercise_a/src/main.rs
+++ hello/excercise_a/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let missiles = 8;
+    let mut missiles = 8;
     let ready = 2;
 
     println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
 }
