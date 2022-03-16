8988f33f54e70b210b92e717c10fe6d771db89b8
Samantha-Grace <sgrace732@gmail.com>
2022-03-16 08:50:01 -0600

    fixed it

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,35 +1,31 @@
 fn main() {
-    // fizzbuzz(15);
-    for x in 1..100{
-        fizzbuzz(x)
-    }
+    fizzbuzz(15);
+    // for x in 1..100{
+    //     fizzbuzz(x)
+    // }
 }
 
 fn fizzbuzz(number: i32) {
-
-    if number % 3 == 0 {
-        println!("fizz");
-    } else if number % 5 == 0  {
-        println!("buzz");
-    } else if number % 3 == 0 && number % 5 ==0 {
+    if number % 3 == 0 && number % 5 == 0 {
         println!("fizzbuzz");
+    } else if number % 5 == 0 {
+        println!("buzz");
+    } else if number % 3 == 0 {
+        println!("fizz");
     } else {
         println!("{}", number);
     }
-    }
-
-
-
+}
 
 // fn main() {
 //     let number = 15;
 
-//     if number % 3 == 0 {
-//         println!("fizz");
+//     if number % 3 == 0 && number % 5 == 0 {
+//         println!("fizzbuzz");
 //     } else if number % 5 == 0  {
 //         println!("buzz");
-//     } else if number % 3 == 0 && number % 5 == 0 {
-//         println!("fizzbuzz");
+//     } else if number % 3 == 0  {
+//         println!("fizz");
 //     } else {
 //         println!("{}", number);
 //     }
